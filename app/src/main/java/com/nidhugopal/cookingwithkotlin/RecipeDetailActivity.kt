85affbdf9eb299ddbd88f9ca9fe18b9e30462049
Nidhu Gopal <nidhu.gopal.p@gmail.com>
2020-05-12 01:13:50 -0700

    detail page added with recipe

--- app/src/main/java/com/nidhugopal/cookingwithkotlin/RecipeDetailActivity.kt
+++ app/src/main/java/com/nidhugopal/cookingwithkotlin/RecipeDetailActivity.kt
@@ -3,8 +3,13 @@ package com.nidhugopal.cookingwithkotlin
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_recipe_detail.*
+import kotlinx.android.synthetic.main.recipe_steps.view.*
 
 class RecipeDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,3 +30,30 @@ class RecipeDetailActivity : AppCompatActivity() {
         }
     }
 }
+
+private class StepsAdapter(
+    val steps: List<String>,
+    val context: Context
+) :
+    RecyclerView.Adapter<StepsViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StepsViewHolder {
+        return StepsViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.recipe_steps,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return steps.count()
+    }
+
+    override fun onBindViewHolder(holder: StepsViewHolder, position: Int) {
+        val step = steps[position]
+        holder.itemView.recipe_step.text = step
+    }
+}
+
+private class StepsViewHolder(view: View) : RecyclerView.ViewHolder(view)
